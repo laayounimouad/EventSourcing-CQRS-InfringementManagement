d5ac4beb317e6c9727ddf2bdd14cade54cce04e9
Laayouni Mouad <50507814+laayounimouad@users.noreply.github.com>
2022-12-25 16:38:17 +0100

    create InfractionService

---
+++ InfractionService/src/main/java/ma/laayouni/InfractionService/InfractionServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.laayouni.InfractionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfractionServiceApplication.class, args);
+	}
+
+}
---
+++ InfractionService/src/test/java/ma/laayouni/InfractionService/InfractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.laayouni.InfractionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
